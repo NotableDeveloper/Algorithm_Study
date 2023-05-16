e0a104bb3a15fbde58259a78f5791a63c425556e
NotableDeveloper <castlereal01@gmail.com>
2023-05-16 14:22:25 +0900

    챕터 10장 1번 문제 테스트 코드 작성 챕터 10장의 1번 문제에 대한 테스트 코드 작성

---
+++ src/test/java/Chap10Test.java
@@ -0,0 +1,12 @@
+import Chap10.Chap10_01;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Chap10Test {
+    @Test
+    void Problem01(){
+        int n = 7;
+        int answer = Chap10_01.solution(n);
+        Assertions.assertEquals(21, answer);
+    }
+}
