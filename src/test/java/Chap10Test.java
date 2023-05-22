1ed4c6b56d7819ebe81201c707bd7f6c20490014
NotableDeveloper <castlereal01@gmail.com>
2023-05-22 14:31:32 +0900

    10-04 테스트 작성 챕터 10장의 4번 문제에 대한 테스트 작성

--- src/test/java/Chap10Test.java
+++ src/test/java/Chap10Test.java
@@ -4,6 +4,9 @@ import Chap10.Chap10_03;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+
 public class Chap10Test {
     @Test
     void Problem01(){
@@ -25,5 +28,19 @@ public class Chap10Test {
         int[] arr = {5, 3, 7, 8, 6, 2, 9, 4};
         int answer = Chap10_03.solution(n, arr);
         Assertions.assertEquals(4, answer);
+    }
+
+    @Test
+    void Problem04(){
+        ArrayList<Brick> bricks = new ArrayList<Brick>();
+
+        bricks.add(new Brick(25, 3, 4));
+        bricks.add(new Brick(4, 4, 6));
+        bricks.add(new Brick(9, 2, 3));
+        bricks.add(new Brick(16, 2, 5));
+        bricks.add(new Brick(1, 5, 2));
+
+        int answer = Chap10_04.solution(bricks);
+        Assertions.assertEquals(10, answer);
     }
 }
